334663bde6e6661e7acf9ab4bd80ae6fcf4b9d19
Ayush Sharma <ayushsharma3310@gmail.com>
2025-02-26 16:41:45 +0530

    Update authentication controller: enhance signup response, improve logout handling, and fix profile picture upload logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -27,7 +27,12 @@ export const signup = async (req, res) => {
             //generate jwt token
             generateToken(newUser._id, res);
             await newUser.save();
-            return res.status(200).json({ message: 'User created successfully' });  
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullname,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -61,26 +66,33 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-    try{
-        res.clearCookie('token');
-        res.status(200).json({ message: 'Logout successful' });
-    }catch(error){
-        res.status(400).json({ message: error.message });
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+      } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
 export const changeProfilePic = async (req, res) => {
     try{
-        const { profilePic } = req.file.path;
+        const { profilePic } = req.body;
         const userId = req.user._id;
         if (!profilePic) {
             return res.status(400).json({ message: 'Please upload a profile picture' });
         }
         const uploadResponse = await cloudinary.uploader.upload(profilePic);
-        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
         if (!updatedUser) {
             return res.status(400).json({ message: 'Profile picture not updated' });
         }
+        
         res.status(200).json(updatedUser);
     }catch (error) {
             res.status(500).json({ message: error.message });
