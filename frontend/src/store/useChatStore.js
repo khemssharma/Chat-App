953449c6873822503540a52eee03c9590f1cf83b
Ayush Sharma <ayushsharma3310@gmail.com>
2025-03-01 15:13:04 +0530

    Update cookie settings for token generation and add logging in chat store

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -14,6 +14,8 @@ export const useChatStore = create((set, get) => ({
     set({ isUsersLoading: true });
     try {
       const res = await axiosInstance.get("/messages/users");
+      console.log("res" + res);
+      
       set({ users: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
